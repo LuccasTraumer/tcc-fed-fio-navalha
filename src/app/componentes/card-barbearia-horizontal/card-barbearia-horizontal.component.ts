6cf9c2a77b2b04da16d1e7737bf45f74b76202d7
Lucas Jesus <cc19372@g.unicamp.br>
2021-06-23 22:26:15 -0300

    criando o card horizontal

--- src/app/componentes/card-barbearia-horizontal/card-barbearia-horizontal.component.ts
+++ src/app/componentes/card-barbearia-horizontal/card-barbearia-horizontal.component.ts
@@ -8,20 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CardBarbeariaHorizontalComponent {
 
   public readonly AVALIACAO_ICON = 'https://raw.githubusercontent.com/LuccasTraumer/tcc-fed-fio-navalha/main/src/assets/icons/star-complete-fill.png?token=AD5NGI3POY7LM53BW53UQ3TA3T5UG';
-  public readonly FAVORITO_ICON = 'https://raw.githubusercontent.com/LuccasTraumer/tcc-fed-fio-navalha/4930202aa2586c1810bb7420ed4a8884132fc099/src/assets/icons/heart-black-empty.svg?token=AD5NGI4KMLEQFNJADQN6JVLA2PHT4';
-  public readonly ICONE_BARBEAIRIA = '';
+  public readonly FAVORITO_ICON = 'https://raw.githubusercontent.com/LuccasTraumer/tcc-fed-fio-navalha/in-progress/src/assets/icons/heart-black-fill.png?token=AD5NGI2V477SGBUUM3TVGJTA3UH4U';
+  public readonly ICONE_BARBEAIRIA = 'https://raw.githubusercontent.com/LuccasTraumer/tcc-fed-fio-navalha/in-progress/src/assets/icons/barbearia_icone_exemplo.jpg?token=AD5NGI7X2PZSKPHUP447SODA3UGUI';
 
   private _avalicaoBarbearia: number = 4.5;
-  private _especialidadeBarbearia = '';
-  private _distanciaBarbearia = '';
+  private _especialidadeBarbearia = 'Black power';
+  private _distanciaBarbearia = 50;
 
   private _margemAtraso = 10;
   private _tempoMedio = 50;
   private _precoMedio = 50;
+  private _nomeBarbearia = 'Roni Barbearia';
 
   constructor() { }
 
-  public distanciaBarbearia(): string {
+  public nomeBarbearia(): string {
+    return this._nomeBarbearia;
+  }
+
+  public distanciaBarbearia() {
     return this._distanciaBarbearia;
   }
 
