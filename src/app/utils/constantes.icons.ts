764d5085c1b6ceac5fb387726a515cf0bbebb5df
Luccas Traumer <lucassj.dev@gmail.com>
2021-06-26 17:14:47 -0300

    Update constantes.icons.ts

--- src/app/utils/constantes.icons.ts
+++ src/app/utils/constantes.icons.ts
@@ -1,4 +1,4 @@
 import { environmentExternalLinks } from '../../environments/enviroment.external.links';
 export class ConstantesIcons {
-  public static readonly LOGO_ICON_PNG = environmentExternalLinks.LOGO_ICON_PNG;
+  public static readonly LOGO_ICON_PNG = 'https://raw.githubusercontent.com/LuccasTraumer/tcc-fed-fio-navalha/main/src/assets/icons/fdn-icon.png?token=AD5NGI64MDQMC4HPYHGLFM3A4DCNU';
 }
