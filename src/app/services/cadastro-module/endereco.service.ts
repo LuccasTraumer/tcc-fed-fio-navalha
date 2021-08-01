3851f600250e4639bd60c071f6d7a270f1496c29
Lucas Jesus <cc19372@g.unicamp.br>
2021-07-31 21:32:49 -0300

    Endereco funcionando

--- src/app/services/cadastro-module/endereco.service.ts
+++ src/app/services/cadastro-module/endereco.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { EMPTY, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
-import { Endereco } from 'src/app/models/Endereco';
 
 
 @Injectable({
@@ -13,20 +11,10 @@ import { Endereco } from 'src/app/models/Endereco';
 export class EnderecoService {
 
   private API_CEP = environment.API_CEP;
-  private endereco;
-
-  httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-  };
 
   constructor(private http: HttpClient) {}
 
   getEndereco(cep: string): Observable<any> {
     return this.http.get(`${this.API_CEP + cep}/json/`);
-  }
-
-  private errorHandler(e: any) : Observable<Endereco> {
-    console.log(e);
-    return EMPTY;
   }
 }
