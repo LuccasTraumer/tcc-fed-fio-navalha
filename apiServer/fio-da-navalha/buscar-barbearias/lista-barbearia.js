9d45864fc373f2fd1b0efdf69ec284235bc0d693
Luccas Traumer <lucas.jeesus@hotmail.com>
2021-09-28 08:56:05 -0300

    Colocando mock na API server

--- apiServer/fio-da-navalha/buscar-barbearias/lista-barbearia.js
+++ apiServer/fio-da-navalha/buscar-barbearias/lista-barbearia.js
@@ -4,9 +4,14 @@ const dummyJsonHelpers = require('../helpers/dummy-json-helpers');
 
 module.exports = (app, endpoint) => {
     app.post(endpoint + '/', (req, res) => {
-        const response = `{
-
-        }`;
+        const response = `[
+            {{#repeat 4}}
+            {
+                "nome":"{{titulo}}"
+                
+            }
+            {{/repeat}}
+        ]`;
         setTimeout(() => {
             // responseModel.error(res, 404, 'Erro ao tentar comunicar com SRV');
             responseModel.sucess(res, response);
