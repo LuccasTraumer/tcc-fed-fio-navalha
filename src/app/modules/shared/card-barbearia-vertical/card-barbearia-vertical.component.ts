4bb92cb2c4fe82023a798778afad0c974ad9f113
Lucas Jesus <cc19372@g.unicamp.br>
2021-10-16 21:48:45 -0300

    Card vertical parcialmente feito

--- src/app/modules/shared/card-barbearia-vertical/card-barbearia-vertical.component.ts
+++ src/app/modules/shared/card-barbearia-vertical/card-barbearia-vertical.component.ts
@@ -11,15 +11,16 @@ export class CardBarbeariaVerticalComponent implements OnInit {
 
   // @Input()
   barbearia: Barbearia;
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.barbearia = new Barbearia();
     this.barbearia.fotoPerfil = ConstantesIcons.BARBEARIA_FOTO;
     this.barbearia.nome = 'Lucas Barbearia';
     this.barbearia.avaliacaoBarbearia = 5.0;
     this.barbearia.tempoMedio = 10;
     this.barbearia.especialidade = 'Cabelo Rasto';
+  }
 
+  ngOnInit(): void {
   }
 
   calcularDistanciaBarbearia() {
